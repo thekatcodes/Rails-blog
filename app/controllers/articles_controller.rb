c1f56838ee9cd4b85d33132476f3b4ffe692db9a
Katie Liu <katieliu59@gmail.com>
2023-03-07 16:13:02 -0500

    Implement show single article code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
     #fetch all articles from the database
     @articles = Article.all
   end
+
+  # show action calls Article.find with the ID captured by the route parameter. The returned article is stored in the @article instance variable, so it is accessible by the view. By default, the show action will render app/views/articles/show.html.erb. 
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+  
+  # get all articles
   get "/articles", to: "articles#index"
 
-  # root "articles#index"
+  #get article by id (:id is the route parameter), This designates a route parameter. A route parameter captures a segment of the request's path, and puts that value into the params Hash, which is accessible by the controller action. For example, when handling a request like GET http://localhost:3000/articles/1, 1 would be captured as the value for :id, which would then be accessible as params[:id] in the show action of ArticlesController.
+  get "/articles/:id", to: "articles#show"
 end
