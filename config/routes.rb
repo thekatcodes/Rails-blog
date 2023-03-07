8727df853fb56b05d0c9421bbc870f83ac10cc06
Katie Liu <katieliu59@gmail.com>
2023-03-07 18:26:40 -0500

    Implement resources method and update links in inex.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
-  
-  # get all articles
-  get "/articles", to: "articles#index"
 
-  #get article by id (:id is the route parameter), This designates a route parameter. A route parameter captures a segment of the request's path, and puts that value into the params Hash, which is accessible by the controller action. For example, when handling a request like GET http://localhost:3000/articles/1, 1 would be captured as the value for :id, which would then be accessible as params[:id] in the show action of ArticlesController.
-  get "/articles/:id", to: "articles#show"
+  # route method in Rails that maps all the conventional routes for a collection of resources (in this case, articles)
+  resources :articles
+
 end
